bc684c31ebfb63bbead481e548474989fafa8631
Leo <leodarkseid@gmail.com>
2023-07-11 16:30:37 +0100

    update to memberManager

---
+++ contracts/membersManager.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "../delegatable-sol/contracts/Delegatable.sol";
+
+contract MembersManager is Ownable, Delegatable {
+}
---
+++ contracts/membersManagerFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+import "./membersManager.sol";
+
+contract MembersManagerFactory {
+    address[] public deployedMembersManagers;
+
+    function createMembersManager() public returns(address){
+        MembersManager newMembersManager = new MembersManager();
+        newMembersManager.transferOwnership(msg.sender);
+
+        deployedMembersManagers.push(address(newMembersManager));
+        return address(newMembersManager);
+    }
+
+    function getDeployedMembersManager() public view returns (address[] memory) {
+        return deployedMembersManagers;
+    }
+}
